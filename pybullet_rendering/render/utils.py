56c34caa83f39ac820fab3d292d8f1157e821553
Igor Kalevatykh <kalevatykh@gmail.com>
2020-02-05 19:40:10 +0100

    fix import

--- pybullet_rendering/render/utils.py
+++ pybullet_rendering/render/utils.py
@@ -1,6 +1,5 @@
-from pathlib import Path
-from pybullet_rendering import ShapeType
-from pybullet_rendering.data import __file__ as data
+from os.path import dirname, join
+import pybullet_rendering as pr
 
 __all__ = ['shape_filename']
 
@@ -8,11 +7,13 @@ __all__ = ['shape_filename']
 def shape_filename(shape):
     if shape.type == ShapeType.Mesh:
         return shape.mesh.filename
-    elif shape.type == ShapeType.Cube:
-        return Path(data).parent / 'primitives' / 'cube.stl'
-    elif shape.type == ShapeType.Sphere:
-        return Path(data).parent / 'primitives' / 'sphere.stl'
-    elif shape.type == ShapeType.Cylinder:
-        return Path(data).parent / 'primitives' / 'cylinder.stl'
-    else:
-        return ''
+
+    packagedir = dirname(pr.__path__[0])
+    if shape.type == pr.ShapeType.Cube:
+        return join(packagedir, 'share', 'primitives', 'cube.stl')
+    elif shape.type == pr.ShapeType.Sphere:
+        return join(packagedir, 'share', 'primitives', 'sphere.stl')
+    elif shape.type == pr.ShapeType.Cylinder:
+        return join(packagedir, 'share', 'primitives', 'cylinder.stl')
+
+    return ''

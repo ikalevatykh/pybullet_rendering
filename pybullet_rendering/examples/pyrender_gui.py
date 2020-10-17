9a2bd5159935e1359724f34c223dff1d2341ec27
ManifoldFR <wilson.jallet@polytechnique.org>
2020-10-17 14:37:25 +0200

    finish Pyrender GUI example

--- pybullet_rendering/examples/pyrender_gui.py
+++ pybullet_rendering/examples/pyrender_gui.py
@@ -1,42 +1,61 @@
+from os import times
 import numpy as np
+import pyrender
 import pybullet as pb
 
 from pybullet_utils.bullet_client import BulletClient
 import pybullet_data
 from pybullet_rendering import RenderingPlugin, BaseRenderer, ShapeType
-from pybullet_rendering.render.pyrender import Loader, PbMaterial, PbCameraNode
+from pybullet_rendering.render.pyrender import Loader, PbLightNode, PbMaterial, PbCameraNode
 from pybullet_rendering.render.utils import shape_filename
 
-import pyrender
 from pyrender import RenderFlags
 
+import time
 from collections import OrderedDict
-
+import pprint
 
 
 class PyrenderGUI(BaseRenderer):
     
     def __init__(self, client: BulletClient) -> None:
         BaseRenderer.__init__(self)
-        self.return_to_bullet = True
+        self.return_to_bullet = False
         
         self.client = client
-        plugin = RenderingPlugin(client, self)
-        self.plugin = plugin
-        
-        self._scene = pyrender.Scene(ambient_light=np.array([1, 1, 1, 1]))
-        self._camera = PbCameraNode(self._scene)
+        self.plugin = RenderingPlugin(client, self)
+
+        self._scene = pyrender.Scene(ambient_light=np.array([0.5, 0.5, 0.5, 1.]))
+        # self._light = PbLightNode(self._scene)
+        # self._camera = PbCameraNode(self._scene)
         self._node_dict = OrderedDict()
         self._loader = Loader()
-        self._flags = pyrender.RenderFlags.NONE
+
+
+        self._flags = pyrender.RenderFlags.RGBA
         self._color, self._depth = None, None
 
-    def get_viewer(self):
+        self.setup_viewer()
+
+    @property
+    def scene(self):
+        return self._scene
+
+    def setup_viewer(self):
+        render_flags = {
+            "shadows": True,
+        }
+        viewer_flags = {
+            "window_title": "Pyrender Bullet GUI",
+        }
         viewer = pyrender.Viewer(self._scene,
-                                 viewport_size=(1920, 1080),
-                                 run_in_thread=True)
-        return viewer
-    
+                                 viewport_size=(1600, 900),
+                                 run_in_thread=True,
+                                 render_flags=render_flags,
+                                 viewer_flags=viewer_flags)
+        self._viewer = viewer
+
+
     def update_scene(self, scene_graph, materials_only):
         """Update a scene graph
 
@@ -44,6 +63,7 @@ class PyrenderGUI(BaseRenderer):
             scene_graph {SceneGraph} -- scene description
             materials_only {bool} -- update only shape materials
         """
+        self._viewer.render_lock.acquire()
         for uid, body in scene_graph.nodes.items():
             node = pyrender.Node(uid)
             self._scene.add_node(node)
@@ -55,12 +75,9 @@ class PyrenderGUI(BaseRenderer):
                     continue
                 model = self._loader.load(filename, body.no_cache)
                 mesh = pyrender.Mesh.from_trimesh(model)
-                if shape.has_material:
-                    material = PbMaterial(shape.material)
-                    for p in mesh.primitives:
-                        p.material = material
                 pose_mat = np.asarray(shape.pose.matrix).reshape(4, 4).T
                 self._scene.add(mesh, pose=pose_mat, parent_node=node)
+        self._viewer.render_lock.release()
 
     def render_frame(self, scene_state, scene_view, frame):
         """Render a scene at scene_state with a scene_view settings
@@ -70,35 +87,53 @@ class PyrenderGUI(BaseRenderer):
             scene_view {SceneView} -- view settings, e.g. camera, light, viewport parameters
             frame {FrameData} -- output image buffer
         """
+        self._viewer.render_lock.acquire()
         for uid, node in self._node_dict.items():
             pose = np.asarray(scene_state.matrix(uid)).reshape(4, 4).T
-            self._scene.set_pose(node, np.asarray(pose))
+            self._scene.set_pose(node, pose)
+        self._viewer.render_lock.release()
 
-        flags = self._flags | RenderFlags.RGBA
+        return False
 
-        if self._camera.is_enabled:
-            self._camera.update(scene_view.camera)
+    def close(self):
+        return self._viewer.close_external()
 
 if __name__ == "__main__":
     client = BulletClient(pb.DIRECT)
-
-
     client.setAdditionalSearchPath(pybullet_data.getDataPath())
-    client.setGravity(*[0, 0, -9.81])
-    
+    grav = np.array([0., 0., -9.91])
+    client.setGravity(*grav)
     gui = PyrenderGUI(client)
-    viewer = gui.get_viewer()
     
-    plane = client.loadURDF("plane.urdf")
-    table = client.loadURDF("table/table.urdf", basePosition=[0.4, 0.04, 1.4])
     
-    t = 0
+    plane = client.loadURDF("plane.urdf", useMaximalCoordinates=True)
+    table = client.loadURDF("table/table.urdf",
+                            basePosition=[0.4, 0., 0.],
+                            flags=pb.URDF_USE_MATERIAL_COLORS_FROM_MTL)
+    ball  = client.loadURDF("soccerball.urdf", basePosition=[0, 0, 1], globalScaling=.25)
+    laikago = client.loadURDF("laikago/laikago.urdf",
+                              basePosition=[0, 1.2, 0.5],
+                              baseOrientation=[1, 0, 0, 1])
     
-    while True:
+    
+    ball_vel = np.array([1.6, 0., 1.])
+    client.resetBaseVelocity(ball, ball_vel)
+    
+    client.getCameraImage(1, 1)
+
+    num_steps = 30 * 2048
+    dt = 1./240
+    client.setTimeStep(dt)
+    render_fps = 30
+    # render every no. of steps
+    num_steps_render = 1. / (dt * render_fps)
+
+    for t in range(num_steps):
+        time.sleep(dt)
         client.stepSimulation()
+        if t % num_steps_render == 0:
+            client.getCameraImage(1, 1)
+            print(f"Time t={t}")
+        
         t += 1
-        print(f"Time t={t}")
-        if t % 30 == 0:
-            input("press enter")
-    
-    
+    gui.close()
